cf5ae8936a9cfa90ed4e4c76036354ce453ce67d
Gerardo Chapa Díaz González <gerardochapa1000@hotmail.com>
2025-04-20 01:10:53 -0600

    fix: remove unused adapter

--- src/adapters/category.adapter.ts
+++
@@ -1,9 +0,0 @@
-import { Category } from "@/interfaces/product.interface";
-import { categoryNames } from "@/utils/product.utils";
-
-export const formatCategories = (categories?: Category[]) => (
-  categories?.map((category) => ({
-    key: category,
-    label: categoryNames[category]
-  }))
-)
